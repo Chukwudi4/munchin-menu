eabcb537175d9a57f21f366771ce2b5a283cab56
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-03-16 01:41:24 +0100

    appjs

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppContainer } from './nav/appcontainer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <AppContainer/>
   );
 }
 
